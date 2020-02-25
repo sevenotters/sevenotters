06a374040c3d74299bd5d6389523c632b6ba29c1
“nicolafiorillo” <nicola.fiorillo@gmail.com>
2020-02-25 15:08:18 +0100

    Update versions.

--- lib/seven/aggregates.ex
+++ lib/seven/aggregates.ex
@@ -3,13 +3,29 @@ defmodule Seven.Aggregates do
 
   @spec get_aggregate(atom, String.t()) :: {:ok, pid}
   def get_aggregate(handler, correlation_value_id) do
-    key = Atom.to_string(handler) <> "_" <> correlation_value_id
-    name = {:via, Registry, {:registry, key}}
+    {key, name} = get_key(handler, correlation_value_id)
 
     case Registry.lookup(:registry, key) do
       [] -> handler.start_link(key, name: name)
       [{pid, _key}] -> {:ok, pid}
       _ -> raise("More values in registry for key #{key}")
     end
+  end
+
+  @spec is_loaded(atom, String.t()) :: nil | pid
+  def is_loaded(handler, correlation_value_id) do
+    {key, _name} = get_key(handler, correlation_value_id)
+
+    case Registry.lookup(:registry, key) do
+      [] -> nil
+      [{pid, _key}] -> pid
+      _ -> raise("More values in registry for key #{key}")
+    end
+  end
+
+  defp get_key(handler, correlation_value_id) do
+    key = Atom.to_string(handler) <> "_" <> correlation_value_id
+    name = {:via, Registry, {:registry, key}}
+    {key, name}
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Seven.Mixfile do
   def project do
     [
       app: :seven,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.1.1",
+      elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
