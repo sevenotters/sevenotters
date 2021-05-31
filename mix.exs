b56c4c129f441f832561b6a9aff66281aa8627de
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2021-05-31 09:53:44 +0200

    Bump to 0.8.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Seven.Mixfile do
   def project do
     [
       app: :seven,
-      version: "0.7.2",
+      version: "0.8.0",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
